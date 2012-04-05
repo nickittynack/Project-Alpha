433ca46b1bf6b68f15a951d62f6ea8724dd5d475
Blair Hudson <blairhudson@me.com>
2012-04-05 11:21:17 +1000

    fixed typo in copyright notice, removed last changed/by as this is tracked by git

--- classes/session.class.php
+++ classes/session.class.php
@@ -1,21 +1,19 @@
 <?php
-# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
-#                                                           #
-#                       Project Alpha                       #
-#                                                           #
-# This file is part of Project Alpha, a PHP rewrite of the  #
-# NSWJLPS system. This project is designed to run on any    #
-# standard implementation of PHP with SQLite.               #
-#                                                           #
-# Do not copy or distribute any portion of this file with   #
-# permission from the original authors.                     #
-#                                                           #
-# Copyright 2012 Benjamin Civitico, Blair Hudson, Nicholas  #
-# Reynolds & Anthony Wales                                  #
-#                                                           #
-# Author: Blair Hudson                                      #
-# Last Modified: 5-4-2012                                   #
-# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+#                                                             #
+#                       Project Alpha                         #
+#                                                             #
+# This file is part of Project Alpha, a PHP rewrite of the    #
+# NSWJLPS system. This project is designed to run on any      #
+# standard implementation of PHP with SQLite.                 #
+#                                                             #
+# Do not copy or distribute any portion of this file without  #
+# permission from the original authors.                       #
+#                                                             #
+# Copyright 2012 Benjamin Civitico, Blair Hudson, Nicholas    #
+# Reynolds & Anthony Wales.                                   #
+#                                                             #
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 
 class session {
     public function __construct(){
--- login.php
+++ login.php
@@ -1,27 +1,21 @@
 <?php
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+#                                                             #
+#                       Project Alpha                         #
+#                                                             #
+# This file is part of Project Alpha, a PHP rewrite of the    #
+# NSWJLPS system. This project is designed to run on any      #
+# standard implementation of PHP with SQLite.                 #
+#                                                             #
+# Do not copy or distribute any portion of this file without  #
+# permission from the original authors.                       #
+#                                                             #
+# Copyright 2012 Benjamin Civitico, Blair Hudson, Nicholas    #
+# Reynolds & Anthony Wales.                                   #
+#                                                             #
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 
-# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
-#                                                           #
-#                       Project Alpha                       #
-#                                                           #
-# This file is part of Project Alpha, a PHP rewrite of the  #
-# NSWJLPS system. This project is designed to run on any    #
-# standard implementation of PHP with SQLite.               #
-#                                                           #
-# Do not copy or distribute any portion of this file with   #
-# permission from the original authors.                     #
-#                                                           #
-# Copyright 2012 Benjamin Civitico, Blair Hudson, Nicholas  #
-# Reynolds & Anthony Wales                                  #
-#                                                           #
-# Author: Blair Hudson                                      #
-# Last Modified: 4-4-2012                                   #
-# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
-
-include("classes/session.class.php");
-
-$session = new session();
-
-echo "Login page";
+echo "Nothing to see yet.";
+exit();
 
 ?>
