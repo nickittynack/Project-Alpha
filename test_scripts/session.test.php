f8ee03f1a95bc2a5085fe79145b13ac220be2803
Blair Hudson <blairhudson@me.com>
2012-04-05 11:09:23 +1000

    added a test page for the session class

---
+++ test_scripts/session.test.php
@@ -0,0 +1,34 @@
+<?php
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+#                                                           #
+#                       Project Alpha                       #
+#                                                           #
+# This file is part of Project Alpha, a PHP rewrite of the  #
+# NSWJLPS system. This project is designed to run on any    #
+# standard implementation of PHP with SQLite.               #
+#                                                           #
+# Do not copy or distribute any portion of this file with   #
+# permission from the original authors.                     #
+#                                                           #
+# Copyright 2012 Benjamin Civitico, Blair Hudson, Nicholas  #
+# Reynolds & Anthony Wales                                  #
+#                                                           #
+# Author: Blair Hudson                                      #
+# Last Modified: 5-4-2012                                   #
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+
+# This file demonstrates how to use session.class.php to
+# login and logout.
+
+include("../classes/session.class.php"); # Include the class
+
+$session = new session(); #Create a new session object
+
+if($session->authd()){ #Returns a true/false if logged in
+	$session->logout(); #End the session, set authd to false
+	echo "You logged out, refresh the page to log in.";
+} else {
+	$session->login(); #Set authd to true
+	echo "You are logged in, refresh the page to log out.";
+}
+?>
